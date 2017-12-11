f7a0b3304ef4e3fff1e73243e58b808100ddc9dc
Marvin Clerge <clergemarvin@gmail.com>
2017-12-10 20:08:35 -0500

    Done

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,41 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(File.read(index_url))
+    doc.css('div .student-card').map do |x|
+      hash = {
+        name: x.css('h4.student-name').text,
+        location: x.css('p.student-location').text,
+        profile_url: x.css('a')[0].values[0]
+      }
+    end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(File.read(profile_url))
+    hash = {}
+
+    #Get social media links
+    doc.css('div.social-icon-container').css('a').each do |social_link|
+      if social_link.values[0].include?('twitter')
+        hash[:twitter] = social_link.values[0]
+      elsif social_link.values[0].include?('linkedin')
+        hash[:linkedin] = social_link.values[0]
+      elsif social_link.values[0].include?('github')
+        hash[:github] = social_link.values[0] if social_link.values[0].include?('github')
+      else
+        hash[:blog] = social_link.values[0]
+      end
+    end
+
+    #Get bio and quote
+    hash[:profile_quote] = doc.css("div.profile-quote").text
+    hash[:bio] = doc.css('div.bio-content.content-holder').text.split()[1..-1].join(' ')
+
+    hash
   end
-
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,30 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |key, value|
+      self.send("#{key}=", value)
+      @@all << self
+    end
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.map do |hash|
+      Student.new(hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key, value|
+      self.send("#{key.to_s}=", value)
+    end
+    self
   end
 
   def self.all
-    
+    @@all.uniq
   end
 end
-
